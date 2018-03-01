ab3e6f441d31f6aeddee391642cf8297e5f3dd63
mRRa <spiriturl@gmail.com>
2018-03-01 21:33:56 +0800

    Add Extension & Razor Helper & Razor function

---
+++ Lib.Mvc.HelperExt/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Lib.Mvc.HelperExt.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// 顯示現在的Controller與Action
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <returns></returns>
+        public static string ShowControllerAndActionName(this HtmlHelper helper)
+        {
+            var currentControllerName =
+                (string)helper.ViewContext.RouteData.Values["controller"];
+
+            var currentActionName =
+                (string)helper.ViewContext.RouteData.Values["action"];
+
+            return string.Format(
+                "<h3>現在的Controller為【{0}】Action為【{1}】</h3>",
+                currentControllerName,
+                currentActionName);
+        }
+
+        /// <summary>
+        /// 確認是否為指定的Controller與Action
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="actionName"></param>
+        /// <param name="controllerName"></param>
+        /// <returns></returns>
+        public static bool IsCurrentAction(this HtmlHelper helper,
+           string actionName,
+           string controllerName)
+        {
+            var currentControllerName =
+                (string)helper.ViewContext.RouteData.Values["controller"];
+
+            var currentActionName =
+                (string)helper.ViewContext.RouteData.Values["action"];
+
+            if (currentControllerName.Equals(controllerName, StringComparison.CurrentCultureIgnoreCase)
+                &&
+                currentActionName.Equals(actionName, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Lib.Mvc.HelperExt/Helpers/StringArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Lib.Mvc.HelperExt.Helpers
+{
+    public static class StringArrayExtensions
+    {
+        /// <summary>
+        /// 嘗試取得陣列中的值
+        /// </summary>
+        /// <param name="ary"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static string TryGetValue(this string[] ary, int index)
+        {
+            return ary?.ElementAtOrDefault(index) ?? "not found";
+        }
+    }
+}
